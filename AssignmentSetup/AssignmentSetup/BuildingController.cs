c071d8c8b7195f90537bd6258ac5c7306225af55
Tt-24 <47601502+Tt-24@users.noreply.github.com>
2020-03-13 17:56:33 +0000

    Lvl 1 implementation and coding is done
    Needs to test
    and write a unit test in Building Controller Tests

---
+++ AssignmentSetup/AssignmentSetup/BuildingController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentSetup
+{
+    public class BuildingController
+    {
+        BuildingController(string id)
+        {
+            buildingID = id.ToLower();
+            currentState = "out of hours";
+        }
+        string buildingID;
+        string currentState;
+
+        string GetCurrentState()
+        {
+            return currentState;
+        }
+
+        bool SetCurrentState(string state)
+        {
+            // set the new currentState
+            if (state == "closed" || state == "out of hours" || state == "open" || state == "fire drill" || state == "fire alarm")
+            {
+                currentState = state;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        string GetBuildingID()
+        {
+            return buildingID;
+        }
+
+        void SetBuildingID(string id)
+        {
+            buildingID = id.ToLower();
+        }
+
+        //string GetStatusReport()
+        //{
+        //    return;
+        //}
+    }
+}
---
+++ AssignmentSetup/AssignmentSetupTests/BuildingControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentSetupTests
+{
+    [TestFixture]
+    public class BuildingControllerTests
+    {
+       [Test]
+       public void fixx
+
+    }
+}
